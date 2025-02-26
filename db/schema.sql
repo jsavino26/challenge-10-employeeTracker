7f3a5d82475bc36cbeef57389032f351a9021508
Jen <jensavino26@gmail.com>
2025-02-26 13:37:20 -0500

    Refining code and resolving errors when running the program

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,19 @@
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    title VARCHAR(255) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role_id INTEGER REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INTEGER REFERENCES employee(id) ON DELETE SET NULL
 );
